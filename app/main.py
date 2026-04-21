99c621e0e3729e6335db1dc1c7adf11a9f5020c1
Alisa Hoys <hoys@protonmail.com>
2026-04-21 17:44:49 +0200

    Corrected clean up file context manager

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from typing import Any
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.file = None
 
     def __enter__(self) -> None:
         return self
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
-        os.remove(self.filename)
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
